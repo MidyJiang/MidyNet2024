4b506bba2c017ee6cdb7b3b33de9cf9775d61823
Midy <70130392+MidyJiang@users.noreply.github.com>
2024-08-28 05:38:59 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,21 @@
 import subprocess
 
 scripts = [
-    # "01_get_myft.py", # 爬取数据，保存成csv
-    # "02_data_processing.py", # 文本分词，保存成txt
-    # "03_get_price.py", # 获取盘内数据，保存成csv
-    "04_dataset.py", # 制作dataset，保存成csv
+    # "01_get_myft.py",
+    # "02_data_processing.py", 
+    # "03_get_price.py", 
+    "04_dataset.py",
     "05_main.py",
     "06+07inmarket.py",
     "08_plot.py",
     "09_sharp_drawdown.py"
-] # 主函数，训练并验证模型，保存在wandb平台
+] # run this as the integrated main function, train the model ,teset it and pot the figures. trace the training on W&B platform.
 
 for script in scripts:
     try:
-        print(f"正在运行 {script}...")
+        print(f"Running {script}...")
         subprocess.run(["python", script], check=True)
-        print(f"{script} 运行成功")
+        print(f"{script} succeeded")
     except subprocess.CalledProcessError as e:
-        print(f"{script} 运行失败: {e}")
+        print(f"{script} Failed at: {e}")
         break
